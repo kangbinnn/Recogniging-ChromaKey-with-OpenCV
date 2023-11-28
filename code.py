9d4690f01d69d17d2469db7d96d56617b3c12150
kangbinnn <138754992+kangbinnn@users.noreply.github.com>
2023-11-28 15:25:17 +0900

    Update code.py (Add White Color

--- code.py
+++ code.py
@@ -28,16 +28,20 @@ def sensingBack(frame):
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV) # HSV로 크로마키 색 인식 할 때 쓸 변수
 
     if func1_check:   # 인식할 크로마키 색: 초록색
-        mask = cv2.inRange(hsv, (45, 100, 0), (75, 255, 255))  # 영상, 최솟값, 최댓값
+        mask = cv2.inRange(hsv, (45, 50, 0), (75, 255, 255))  # 영상, 최솟값, 최댓값
         textOption(frame, "Chromakey: Green", 2)
 
     elif func2_check: # 인식할 크로마키 색: 검은색
         mask = cv2.inRange(frame, (0, 0, 0), (60, 60, 60))   # 영상, 최솟값, 최댓값
         textOption(frame, "Chromakey: Black", 2)
 
-    elif func3_check: # 인식할 크로마키 색: 초록색
+    elif func3_check: # 인식할 크로마키 색: 파란색
         mask = cv2.inRange(hsv, (90, 130, 0), (120, 255, 255)) # 영상, 최솟값, 최댓값
         textOption(frame, "Chromakey: Blue", 2)
+    
+    elif func4_check: # 인식할 크로마키 색: 흰색
+        mask = cv2.inRange(hsv, (0, 0, 190), (255, 60, 255)) # 영상, 최솟값, 최댓값
+        textOption(frame, "Chromakey: White", 2)
 
     return mask
 
@@ -65,6 +69,7 @@ do_composit = False # False 면 합성을 안함. True면 크로마키 합성
 func1_check = True  # 기본값
 func2_check = False
 func3_check = False
+func4_check = False
 back1_check = True  # 기본값
 back2_check = False
 back3_check = False
@@ -81,7 +86,7 @@ while True: # 무한 루프
     ret2, frame2 = cap1.read() # 원본 영상도 띄우기 위해
 
     textOption(frame1, "[SPACE]: on/off", 27)
-    textOption(frame1, "[Q/W/E]: chromakey color", 28)
+    textOption(frame1, "[Q/W/E/R]: chromakey color", 28)
     textOption(frame1, "[Z/X/C]: background type", 29)
 
     # do_composit 플래그가 True일 때에만 합성
@@ -106,13 +111,16 @@ while True: # 무한 루프
         # 인식할 크로마키 색깔 플래그 설정
         if key == ord('q'):
             func1_check = True
-            func2_check = func3_check = False
+            func2_check = func3_check = func4_check = False
         elif key == ord('w'):
             func2_check = True
-            func1_check = func3_check = False
+            func1_check = func3_check = func4_check = False
         elif key == ord('e'):
             func3_check = True
-            func1_check = func2_check = False
+            func1_check = func2_check = func4_check = False
+        elif key == ord('r'):
+            func4_check = True
+            func1_check = func2_check = func3_check = False
         # 배경 색깔 플래그 설정
         if key == ord('z'):
             back1_check = True
